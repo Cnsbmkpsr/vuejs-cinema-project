19ca8294958e58b826418dbcae80e2a9bd9efc05
Louis Lacoste <moi@louislacoste.fr>
2023-04-25 16:19:38 +0200

    feat: styled the full movie item component

--- src/features/movie/components/MovieListItem.vue
+++ src/features/movie/components/MovieListItem.vue
@@ -1,11 +1,32 @@
 <!-- MovieListItem.vue -->
 <template>
-  <div class="movie-list-item">
-    <img :src="'https://image.tmdb.org/t/p/w200/' + movie.poster_path" alt="Movie Poster" />
-    <h2>{{ movie.title }}</h2>
-    <p>Rating: {{ movie.vote_average }}</p>
-    <p>Release Date: {{ movie.release_date }}</p>
-    <p>{{ movie.overview }}</p>
+  <div class="py-16 col-span-2 lg:col-span-1 sm:max-w-xl sm:mx-auto">
+    <div
+      class="bg-white shadow-lg border-gray-100 max-h-80 border sm:rounded-3xl p-8 flex space-x-8"
+    >
+      <div class="h-48 overflow-visible w-1/2">
+        <img
+          class="rounded-3xl shadow-lg"
+          :src="'https://image.tmdb.org/t/p/w300/' + movie.poster_path"
+          alt="Movie Poster"
+        />
+      </div>
+      <div class="flex flex-col w-1/2 space-y-4">
+        <div class="flex justify-between items-start">
+          <h2 class="text-3xl font-bold">{{ movie.title }}</h2>
+          <div class="bg-yellow-400 font-bold rounded-xl p-2">{{ movie.vote_average }}</div>
+        </div>
+        <div>
+          <div class="text-sm text-gray-400">{{ movie.media_type }}</div>
+          <div class="text-lg text-gray-800">
+            {{ movie.release_date?.substring(0, 4) }}
+          </div>
+        </div>
+        <p class="text-gray-400 max-h-40 overflow-y-hidden">
+          {{ movie.overview }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,8 +43,3 @@ export default defineComponent({
   }
 })
 </script>
-
-<style scoped>
-.movie-list-item {
-}
-</style>
