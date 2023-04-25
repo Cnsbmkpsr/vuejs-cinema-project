bf315fc7dead468e6fed7ca7ca76f2665686ade4
Louis Lacoste <moi@louislacoste.fr>
2023-04-25 16:19:07 +0200

    refactor: moved MovieList component to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,31 @@
-<script setup lang="ts"></script>
+<template>
+  <h1 class="text-3xl">Movie List</h1>
+  <div class="grid grid-cols-2" ref="moviesListElement">
+    <MovieListItem v-for="movie in movieStore.movies" :key="movie.id" :movie="movie" />
+  </div>
+</template>
 
-<template>Homepage</template>
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+import MovieListItem from '../features/movie/components/MovieListItem.vue'
+import { useMovieStore } from '../features/movie/store'
+import { onScrollBottom$ } from '../features/movie/utils/onScrollBottom'
+
+const moviesListElement = ref<HTMLElement>()
+
+const movieStore = useMovieStore()
+
+movieStore.loadMovies()
+
+watchEffect(() => {
+  if (!moviesListElement.value) return
+
+  const scrollButtonSubscription = onScrollBottom$(moviesListElement.value).subscribe(() => {
+    movieStore.fetchNextPage()
+  })
+
+  return () => {
+    scrollButtonSubscription.unsubscribe()
+  }
+})
+</script>
