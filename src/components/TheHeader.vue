1c868c2d940a70f01bff9ac5a70d0d82fd4b2196
Louis Lacoste <moi@louislacoste.fr>
2023-04-26 15:36:43 +0200

    feat: added the header component for the app

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-white rounded-[48px] w-full px-12 py-10 flex justify-between">
+    <h1 class="text-7xl">{{ props.title }}</h1>
+    <div class="">
+      <slot name="search" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+}>()
+</script>
+
+<style scoped></style>
