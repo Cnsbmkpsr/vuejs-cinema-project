199f1c27d9990513890031d738f4e8528099b59f
Louis Lacoste <moi@louislacoste.fr>
2023-04-27 19:58:16 +0200

    refactor: moved router file to the src's root

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import MoviesDetailsView from '../views/MoviesDetailsView.vue'
+import HomeView from './views/HomeView.vue'
+import MoviesDetailsView from './views/MoviesDetailsView.vue'
 
 export default createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
