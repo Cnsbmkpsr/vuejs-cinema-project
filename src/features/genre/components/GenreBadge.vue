59bf01e2dd23306849e4f573ad20aca0ae718b16
Louis Lacoste <moi@louislacoste.fr>
2023-04-27 12:29:05 +0200

    feat: added new genre badge with responsiveness

---
+++ src/features/genre/components/GenreBadge.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    @click="handleToggleGenre()"
+    :class="[
+      'rounded-full text-gray-950 flex items-center border-[3px] shrink-0 text-2xl pr-2 pl-2.5 py-1 gap-2 hover:cursor-pointer',
+      props.isSelected ? 'bg-white border-white' : 'bg-transparent-full border-black'
+    ]"
+  >
+    {{ props.title }}
+    <img
+      :src="props.isSelected ? '/imgs/close-badge-icon.svg' : '/imgs/add-badge-icon.svg'"
+      class="h-6 w-6"
+      alt="Badge action icon"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useGenreStore } from '../store'
+
+const props = defineProps<{
+  title: string
+  isSelected: boolean
+}>()
+
+const genreStore = useGenreStore()
+
+const handleToggleGenre = () => {
+  if (props.isSelected) {
+    genreStore.unselectGenre(props.title)
+  } else {
+    genreStore.selectGenre(props.title)
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ src/features/genre/components/GenresList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex gap-2 overflow-x-auto pb-0.5">
+    <GenreBadge
+      v-for="genre in genresStore.genres"
+      :key="genre"
+      :title="genre"
+      :isSelected="genresStore.isSelected(genre)"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useGenreStore } from '../store'
+import GenreBadge from './GenreBadge.vue'
+
+const genresStore = useGenreStore()
+</script>
+
+<style scoped></style>
